af1f132f83fc6f5220acfc23ec8bb4ce6b6c2822
Kane Koo <kanekoo@gmail.com>
2019-08-15 21:39:51 -0400

    commit takes byebug out of validations; also due to MacBook instability

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+
     @user = User.find_by(name: params[:name])
   #  binding.pry
     if @user == "" or @user == nil
       redirect_to new_session_path, alert: "no such user"
     else
       # binding.pry
-      return  head(:forbidden) unless @user.authenticate(params[:password])
+      return head(:forbidden) unless @user.authenticate(params[:password])
       # binding.pry
       session[:user_id] = @user.id
       redirect_to user_path(@user.id)
